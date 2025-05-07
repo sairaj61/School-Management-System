65a32a9d3e1ba89890982186be5bb9c31b85d490
sksairaj <rahul.kamboj@skutally.com>
2025-05-07 09:12:46 +0530

    updated the methods

--- backend/routers/dashboard.py
+++ backend/routers/dashboard.py
@@ -1,4 +1,7 @@
 from fastapi import APIRouter, Depends
+
+from auth.auth_model import User
+from auth.auth_service import current_active_user
 from services.dashboard_service import DashboardService
 from database import get_db
 
@@ -6,6 +9,6 @@ router = APIRouter()
 
 
 @router.get("/")
-async def get_dashboard(db=Depends(get_db)):
+async def get_dashboard(db=Depends(get_db), user: User = Depends(current_active_user)):
     service = DashboardService(db)
     return await service.get_dashboard()
