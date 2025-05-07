881f73a8339941b591ac953eab9eed3b580e2922
sksairaj <rahul.kamboj@skutally.com>
2025-05-07 16:07:34 +0530

    added docker config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Allow Vite to accept external connections
+    port: 5173, // Ensure Vite runs on port 5173
+  },
 });
